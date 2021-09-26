6f3e84d67a5af6310f3258f640fb966cef73850d
BrunoNaves07 <bruno.naves@estudante.ufla.br>
2021-09-26 10:54:35 -0300

    Alteração de cores e alteração na resposta do resultado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -19,7 +19,16 @@ export class HomePage {
 
   onCalculate() {
     const imc = this.weight / (this.height * this.height);
-    this.showMessage(`IMC = ${imc.toFixed(2)}`);
+    if(imc < 18.5) {
+      this.showMessage(`IMC = ${imc.toFixed(2)} - Magreza`);
+    } else if(imc >= 18.5 && imc <= 24.9) {
+      this.showMessage(`IMC = ${imc.toFixed(2)} - Normal`);
+    } else if(imc > 24.9 && imc <= 30) {
+      this.showMessage(`IMC = ${imc.toFixed(2)} - Sobrepeso`);
+    } else if(imc > 30) {
+      this.showMessage(`IMC = ${imc.toFixed(2)} - Obesidade`);
+    }
+
   }
 
   async showMessage(msg: string) {
